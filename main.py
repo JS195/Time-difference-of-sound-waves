4709392ce0f8098841fdc53a15cc068271cf824b
Josh Smith <de20812@bristol.ac.uk>
2023-02-02 15:23:17 +0000

    Formatting corrections

--- main.py
+++ main.py
@@ -1,56 +1,54 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import random
 import pandas as pd
-import sklearn as sk
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
 def deltat(cp1, cs2, cp2):
-  d = 100000
-  theta_p1 = np.linspace(0, np.pi/2, num=100)
-  g2 = cp2/cp1
-  g1 = cs2/cp1
-  delta_t1 = np.array([(d/cp1)*(np.cos(theta) + 1/np.sqrt(1-g1**2*np.sin(theta)**2)*(g1*np.sin(theta)**2-1/g1)) for theta in theta_p1]) 
-  delta_t2 = np.array([(d/cp1)*(np.cos(theta) + 1/np.sqrt(1-g2**2*np.sin(theta)**2)*(g2*np.sin(theta)**2-1/g2)) for theta in theta_p1])
-  delta_t3 = delta_t2-delta_t1
-  return(delta_t3)
+    d = 100000
+    theta_p1 = np.linspace(0, np.pi/2, num=100)
+    g2 = cp2/cp1
+    g1 = cs2/cp1
+    delta_t1 = np.array([(d/cp1)*(np.cos(theta) + 1/np.sqrt(1-g1**2*np.sin(theta)**2)*(g1*np.sin(theta)**2-1/g1)) for theta in theta_p1]) 
+    delta_t2 = np.array([(d/cp1)*(np.cos(theta) + 1/np.sqrt(1-g2**2*np.sin(theta)**2)*(g2*np.sin(theta)**2-1/g2)) for theta in theta_p1])
+    delta_t3 = delta_t2-delta_t1
+    return(delta_t3)
 
 def plotter(delta_t3):
-  theta_p1 = np.linspace(0, np.pi/2, num=100)
-  plt.plot(theta_p1, delta_t3,'r')
-  plt.xlabel('theta_p1 (rad)')
-  plt.ylabel('Time (s)')
-  plt.title('Difference in time between longitudinal and shear waves reaching the reciever for a variety of angles')
-  plt.show()
+    theta_p1 = np.linspace(0, np.pi/2, num=100)
+    plt.plot(theta_p1, delta_t3,'r')
+    plt.xlabel('theta_p1 (rad)')
+    plt.ylabel('Time (s)')
+    plt.title('Difference in time between longitudinal and shear waves reaching the reciever for a variety of angles')
+    plt.show()
 
 # Values found from https://www.engineeringtoolbox.com/sound-speed-solids-d_713.html
 #Steel
-cp1 = 1500
-cs2 = 3150
-cp2 = 5800
-#steel = deltat(cp1, cs2, cp2)
+steel_cp1 = 1500
+steel_cs2 = 3150
+steel_cp2 = 5800
+steel = deltat(steel_cp1, steel_cs2, steel_cp2)
 #plotter(steel)
 
 #Brass: 
-cp1 = 1500
-cs2 = 2110
-cp2 = 4700
-#brass = deltat(cp1, cs2, cp2)
+brass_cp1 = 1500
+brass_cs2 = 2110
+brass_cp2 = 4700
+brass = deltat(brass_cp1, brass_cs2, brass_cp2)
 #plotter(brass)
 
 #Concrete
-cp1 = 1500
-cs2 = 3200
-cp2 = 3700
-#concrete = deltat(cp1, cs2, cp2)
+concrete_cp1 = 1500
+concrete_cs2 = 3200
+concrete_cp2 = 3700
+concrete = deltat(concrete_cp1, concrete_cs2, concrete_cp2)
 #plotter(concrete)
 
 # Lead (rolled)
-cp1 = 1500
-cs2 = 690
-cp2 = 1960
-#lead = deltat(cp1, cs2, cp2)
+lead_cp1 = 1500
+lead_cs2 = 690
+lead_cp2 = 1960
+lead = deltat(lead_cp1, lead_cs2, lead_cp2)
 #plotter(lead)
 
 def noise(delta_t1):
@@ -83,14 +81,14 @@ df = (pd.DataFrame(np.array(noisysteel+noisybrass+noisyconcrete+noisylead))).dro
 scaled_df = StandardScaler().fit_transform(df)
 
 def Kmeans_method(filename,num_components):
-  new_cluster = np.array(filename)
-  kmeans = KMeans(n_clusters= num_components, random_state=0)
-  label = kmeans.fit_predict(new_cluster)
-  centroids = kmeans.cluster_centers_
-  for i in range(0,4):
-    plt.scatter(new_cluster[label == i , 0] ,new_cluster[label == i , 1], label = i, s = 10)
-    plt.scatter(centroids[:,0] , centroids[:,1] , s = 30, color = 'k', marker="x")
-    plt.legend()
-  return label
+    new_cluster = np.array(filename)
+    kmeans = KMeans(n_clusters= num_components, random_state=0)
+    label = kmeans.fit_predict(new_cluster)
+    centroids = kmeans.cluster_centers_
+    for i in range(0,4):
+        plt.scatter(new_cluster[label == i , 0] ,new_cluster[label == i , 1], label = i, s = 10)
+        plt.scatter(centroids[:,0] , centroids[:,1] , s = 30, color = 'k', marker="x")
+        plt.legend()
+    return label
 
 Kmeans_method(scaled_df,4)
